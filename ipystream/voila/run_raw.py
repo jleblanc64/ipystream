31d6084ccacbe93ae8a5719977dbbe5fa743e91c
charles.dabadie <charles.dabadie@sympheny.com>
2026-01-06 18:39:23 +0100

    prepare 0.1.8

--- ipystream/voila/run_raw.py
+++ ipystream/voila/run_raw.py
@@ -5,9 +5,9 @@ from ipystream.voila import patched_generator, auth_wall_limit, patch_voila
 from ipystream.voila.utils import create_ipynb
 
 
-def run(disable_logging, POOL_SIZE=2, use_xpython=False):
-    patched_generator.patch_voila_get_generator(enforce_PARAM_KEY_TOKEN=False)
-    auth_wall_limit.patch(log_user_fun=None, token_to_user_fun=None)
+def run(disable_logging, POOL_SIZE=2, use_xpython=False, enforce_PARAM_KEY_TOKEN=False, log_user_fun=None, token_to_user_fun=None):
+    patched_generator.patch_voila_get_generator(enforce_PARAM_KEY_TOKEN)
+    auth_wall_limit.patch(log_user_fun, token_to_user_fun)
 
     NOTEBOOK = "jupyter.ipynb"
 
