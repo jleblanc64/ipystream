e27515b1f6437798c486f28349ff9e0cad02d88e
charles.dabadie <charles.dabadie@sympheny.com>
2026-01-06 19:15:07 +0100

    extra_args_override

--- ipystream/voila/run_raw.py
+++ ipystream/voila/run_raw.py
@@ -5,7 +5,7 @@ from ipystream.voila import patched_generator, auth_wall_limit, patch_voila
 from ipystream.voila.utils import create_ipynb
 
 
-def run(disable_logging, POOL_SIZE=2, use_xpython=False, enforce_PARAM_KEY_TOKEN=False, log_user_fun=None, token_to_user_fun=None):
+def run(disable_logging, POOL_SIZE=2, use_xpython=False, enforce_PARAM_KEY_TOKEN=False, log_user_fun=None, token_to_user_fun=None, extra_args_override=None):
     patched_generator.patch_voila_get_generator(enforce_PARAM_KEY_TOKEN)
     auth_wall_limit.patch(log_user_fun, token_to_user_fun)
 
@@ -22,6 +22,9 @@ def run(disable_logging, POOL_SIZE=2, use_xpython=False, enforce_PARAM_KEY_TOKEN
         "--preheat_kernel=True",
         f"--pool_size={POOL_SIZE}",
     ]
+
+    if extra_args_override:
+        extra_args = extra_args_override
 
     create_ipynb(NOTEBOOK, use_xpython)
     sys.argv = ["voila", NOTEBOOK] + extra_args
