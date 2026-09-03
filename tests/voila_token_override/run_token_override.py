2ec11bed3f1a8eb7eb5837c266accf22b54d27b6
jleblanc64 <dabadich@gmail.com>
2026-09-03 08:27:16 +0200

    cleanup

--- tests/voila_token_override/run_token_override.py
+++ tests/voila_token_override/run_token_override.py
@@ -4,8 +4,10 @@ from ipystream.voila.utils import OS_JWT_OVERRIDE
 from tests.voila_token_override.notebook import load_creds
 import requests as r
 
-u, p = load_creds("/home/charles/Desktop/SEP.properties")
-url = "https://eu-north-1-api.dev.sympheny.com/backoffice/auth/ext/token"
-os.environ[OS_JWT_OVERRIDE] = r.post(url, json={"email": u, "password": p}).json()["access_token"]
+username, password = load_creds("/home/charles/Desktop/SEP.properties")
+base_url = "https://eu-north-1-api.dev.sympheny.com/"
+
+os.environ[OS_JWT_OVERRIDE] = r.post(f"{base_url}backoffice/auth/ext/token",
+                                     json={"email": username, "password": password}).json()["access_token"]
 
 ipystream.run()
